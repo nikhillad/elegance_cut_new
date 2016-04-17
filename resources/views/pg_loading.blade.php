3196e1091b3c48ba99363334a8fbc95fc197fabb
Nikhil Lad <nikhil.lad@outlook.com>
2016-04-17 22:29:51 +0530

    solved bug

--- resources/views/pg_loading.blade.php
+++ resources/views/pg_loading.blade.php
@@ -1,11 +1,18 @@
 <html>
+	<!-- CSS -->
 	<link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+	
+	<!-- JS -->
+	<script src="{{asset('js/jquery-latest.min.js')}}"></script>
+
+	<!-- Loading Object -->
 	<div style="width:100%;text-align:center;margin-top:20%">
 		<img class="anim" src="{{asset('images/loader.gif')}}" alt="loading..." />
 		<br>
 		<h2 style="font-family: 'Lato', sans-serif;font-weight: 100">Redirecting to payment gateway</h2>
 	</div>
 
+	<!-- Form -->
 	<form id="pay" method="post" action="{{Config('global.payu.initiate_txn_url')}}">
 		<input type="hidden" name="txnid" value="{{$objTxnMaster->txn_id}}">
 		<input type="hidden" name="amount" value="{{$objTxnMaster->amount}}">
@@ -19,7 +26,6 @@
 		<input type="hidden" name="curl" value="{{route('curl')}}">
 		<input type="hidden" name="hash" value="{{$hash}}">
 		<input type="hidden" name="key" value="{{Config('global.payu.key')}}">
-		<input type="hidden" name="salt" value="{{Config('global.payu.salt')}}">
 	</form>
 	
 	<script type="text/javascript">
